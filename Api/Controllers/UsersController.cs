2309568c6e95dcad65cc6ee598708ffc700001dc
Gregor Hutterer <gregor.hutterer@hotmail.com>
2023-01-06 16:59:14 +0100

    Starting with Fronted

--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
-    [Authorize]  
+   // [Authorize]  
     public class UsersController : BaseApiController
     {
         private readonly DataContext context;
