285edf91a115836b1bebe7f96756732128fcac50
Gregor Hutterer <gregor.hutterer@hotmail.com>
2023-01-10 18:46:06 +0100

    Progress

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { SubscriptionLog } from 'rxjs/internal/testing/SubscriptionLog';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +15,27 @@ export class AppComponent implements OnInit {
   users: any;
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
-      error : () => {console.log(Error)},
-      complete: () => {console.log("Request has completed") }
-      })
+      error: () => { console.log(Error) },
+      complete: () => { console.log("Request has completed") }
+    })
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 
 }
